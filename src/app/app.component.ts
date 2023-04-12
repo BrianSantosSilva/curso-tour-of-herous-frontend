dc917b3f3c6343b2d1e398b346b2c5de1ccec871
Brian Santos <briam.a.s.u.s@gmail.com>
2023-04-11 23:34:02 -0300

    limpa a tela inicial do projeto e coloca titulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'curso-tour-of-herous-frontend';
+  title = 'Tour of Herous';
 }
